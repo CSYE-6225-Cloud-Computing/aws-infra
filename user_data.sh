447c72daa31a047b935749e3389a492d03f4a4a1
SaiMahith Chigurupati <chigurupati.sa@northeastern.edu>
2023-03-20 22:06:34 -0400

    updated scripts for cloud watch

--- user_data.sh
+++ user_data.sh
@@ -12,11 +12,26 @@ AWS_BUCKET_NAME=${s3_bucket}
 AWS_BUCKET_REGION=${s3_region}
 
 PORT="8080"
+
 EOF
+
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:/opt/cloudwatch-config.json \
+    -s
+
+
+sudo systemctl enable amazon-cloudwatch-agent.service # ***********************************
 
 sudo systemctl daemon-reload
 
+# webapp service start
 sudo systemctl enable webapp.service
 sudo systemctl start webapp.service
-
 sudo systemctl status webapp.service
+
+# cloud watch agent service start
+sudo systemctl enable awslogsd # ********************************************
+sudo systemctl start awslogsd
+sudo systemctl status awslogsd
