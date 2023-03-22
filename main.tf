577d797098aa3dac9a0d07f177231681ddd8f240
SaiMahith Chigurupati <chigurupati.sa@northeastern.edu>
2023-03-21 23:40:37 -0400

    updated

--- main.tf
+++ main.tf
@@ -226,6 +226,10 @@ resource "aws_iam_role_policy_attachment" "webapp_s3_policy_attachment" {
   role       = aws_iam_role.ec2_csye6225.name
 }
 
+resource "aws_iam_role_policy_attachment" "cloudwatch_policy_attachment" {
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+  role       = aws_iam_role.ec2_csye6225.name
+}
 
 resource "aws_iam_instance_profile" "web_instance_profile" {
   name = "web_instance_profile"
